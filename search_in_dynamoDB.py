485965173fe1840cbe26d5bde68879d6b855c9ea
shear99 <dancheong99@gmail.com>
2022-11-15 23:22:17 +0900

    delete annotation

--- search_in_dynamoDB.py
+++ search_in_dynamoDB.py
@@ -48,30 +48,3 @@ def lambda_handler(event, context):
         'statusCode' : 200,
         'body' : json.dumps(holiday_info)
     }
-
-'''
-Test Event Name
-test
-
-Response
-{
-  "statusCode": 200,
-  "body": "{\"Responses\": {\"Holiday\": [{\"holiday\": {\"S\": \"\\uac1c\\ucc9c\\uc808\"}, \"date\": {\"N\": \"2210\"}, \"sortdate\": {\"N\": \"221003\"}}]}, \"UnprocessedKeys\": {}, \"ResponseMetadata\": {\"RequestId\": \"JRQ9IELPKGGFQGB3LP731BA1SFVV4KQNSO5AEMVJF66Q9ASUAAJG\", \"HTTPStatusCode\": 200, \"HTTPHeaders\": {\"server\": \"Server\", \"date\": \"Tue, 15 Nov 2022 14:03:23 GMT\", \"content-type\": \"application/x-amz-json-1.0\", \"content-length\": \"124\", \"connection\": \"keep-alive\", \"x-amzn-requestid\": \"JRQ9IELPKGGFQGB3LP731BA1SFVV4KQNSO5AEMVJF66Q9ASUAAJG\", \"x-amz-crc32\": \"178670077\"}, \"RetryAttempts\": 0}}"
-}
-
-Function Logs
-START RequestId: 8196a990-7309-4ded-97c6-e236c49ebcde Version: $LATEST
-221000
-list1 is empty
-221001
-list1 is empty
-221002
-list1 is empty
-221003
-[{'holiday': {'S': '개천절'}, 'date': {'N': '2210'}, 'sortdate': {'N': '221003'}}]
-END RequestId: 8196a990-7309-4ded-97c6-e236c49ebcde
-REPORT RequestId: 8196a990-7309-4ded-97c6-e236c49ebcde	Duration: 1476.14 ms	Billed Duration: 1477 ms	Memory Size: 128 MB	Max Memory Used: 66 MB	Init Duration: 251.00 ms
-
-Request ID
-8196a990-7309-4ded-97c6-e236c49ebcde
-'''
